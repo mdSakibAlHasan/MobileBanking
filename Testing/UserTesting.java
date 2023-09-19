6b6f66cf7e309b548c1c9ae2c84883356b36476c
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2023-09-19 13:04:32 +0600

    testing part initiate

---
+++ Testing/UserTesting.java
@@ -0,0 +1,17 @@
+package new_banking.Testing;
+
+import new_banking.User_details;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTesting {
+    User_details userDetails = new User_details();
+
+    @Test
+    @DisplayName("cash added")
+    void addMoneyInBkash(){
+        assertEquals("added",userDetails.user_details_programe());
+    }
+}
